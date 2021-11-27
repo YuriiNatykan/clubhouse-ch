823e472a0fa8b26123b0ed233da2b536dd887ca9
YuriiNatykan <yurikdnuk3@gmail.com>
2021-11-27 18:37:27 +0200

    3_2.getServerSideProps

--- pages/rooms.tsx
+++ pages/rooms.tsx
@@ -3,8 +3,18 @@ import { Button } from '../components/Button';
 import { ConversationCard } from '../components/ConversationCard';
 import { Header } from '../components/Header';
 import Link from 'next/link';
+import Axios from '../core/axios';
 
-function rooms() {
+export default function RoomsPage({ rooms = [] }) {
+  // const [rooms, setRooms] = React.useState([]);
+
+  // React.useEffect(() => {
+  //   (async () => {
+  //     const { data } = await Axios.get('/rooms.json');
+  //     setRooms(data);
+  //   })();
+  // }, []);
+
   return (
     <>
       <Header avatarUrl="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSWb0scT2J6qrIzEaSqw8l4OPB7Bi1wWR_pXQ&usqp=CAU" />
@@ -13,25 +23,41 @@ function rooms() {
           <h1>All conversations</h1>
           <Button color="green">+ Start room</Button>
         </div>
-        <div className="mt-20">
-          <Link href="/rooms/test-room">
-            <a>
-              <ConversationCard
-                title="Создаю клон"
-                guests={['Anatoliy, Tamara']}
-                avatars={[
-                  'https://images.unsplash.com/photo-1599566150163-29194dcaad36?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8YXZhdGFyfGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=600&q=60',
-                  'https://images.unsplash.com/photo-1494790108377-be9c29b29330?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8NHx8YXZhdGFyfGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=600&q=60',
-                ]}
-                guestsCount={4}
-                speakersCount={1}
-              />
-            </a>
-          </Link>
+        <div className="grid mt-30">
+          {rooms.map((obj) => (
+            <Link key={obj.id} href={`/rooms/${obj.id}`}>
+              <a className="d-flex">
+                <ConversationCard
+                  title={obj.title}
+                  guests={obj.guests}
+                  avatars={obj.avatars}
+                  guestsCount={obj.guestsCount}
+                  speakersCount={obj.speakersCount}
+                />
+              </a>
+            </Link>
+          ))}
         </div>
       </div>
     </>
   );
 }
 
-export default rooms;
+//запрашивать все данные сразу в next js на сервере (useEffect запрашивает данные в бразуре, после отображение HTML)
+export const getServerSideProps = async () => {
+  try {
+    const { data } = await Axios.get('/rooms.json');
+    return {
+      props: {
+        rooms: data,
+      },
+    };
+  } catch (error) {
+    console.log('Error');
+    return {
+      props: {
+        rooms: {},
+      },
+    };
+  }
+};
--- pages/rooms/[id].tsx
+++ pages/rooms/[id].tsx
@@ -1,24 +1,39 @@
-import { useRouter } from 'next/dist/client/router';
 import React from 'react';
 import { BackButton } from '../../components/BackButton';
-import { Button } from '../../components/Button';
 import { Header } from '../../components/Header';
+import Axios from '../../core/axios';
 
-import { Profile } from '../../components/Profile';
 import { Room } from '../../components/Room';
 
-export default function RoomPage() {
-  const router = useRouter();
-  const { id } = router.query;
-
+export default function RoomPage({ room }) {
   return (
     <>
       <Header avatarUrl="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSWb0scT2J6qrIzEaSqw8l4OPB7Bi1wWR_pXQ&usqp=CAU" />
       <div className="container mt-40">
         <h2>All rooms</h2>
         <BackButton title="Back" href="/rooms" />
-        <Room title="Название комнаты" />
+        <Room title={room.title} />
       </div>
     </>
   );
 }
+
+export const getServerSideProps = async (context) => {
+  try {
+    const { data } = await Axios.get('/rooms.json');
+    const roomId = context.query.id;
+    const room = data.find((obj) => obj.id === roomId);
+    return {
+      props: {
+        room: room,
+      },
+    };
+  } catch (error) {
+    console.log('Error');
+    return {
+      props: {
+        rooms: {},
+      },
+    };
+  }
+};
